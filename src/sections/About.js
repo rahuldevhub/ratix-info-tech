0be010dbc1427fee67786e4b100106d58501e99e
Rahul <rahul.reveldor@gmail.com>
2024-03-23 09:33:16 +0530

    Update About.js

--- src/sections/About.js
+++ src/sections/About.js
@@ -26,7 +26,7 @@ console.log(active)
             <p className='about-title' data-aos="fade-left">Secret behind our client<br /> Satisfaction</p>
             <p className='about-subtitle' data-aos="zoom-in">Ratix Info Tech offers a wide range of services to help businesses grow and succeed in the digital age. Our mission is to provide innovative and customized solutions that meet the needs and expectations of our clients.
             
-            <button type='button' onClick={()=> (handlechange())} className='aboutseemore'>See More</button>
+            // <button type='button' onClick={()=> (handlechange())} className='aboutseemore'>See More</button>
 
 <br/><br/> We use the latest tools and technologies to ensure that our products are user-friendly, secure, and scalable. We also offer ongoing support and maintenance to ensure that our clients get the best value for their money.
 <br/><br/>Leading companies recognize our unwavering commitment to quality, innovation, and client satisfaction. When you partner with Ratix, you can rest assured that you are in safe hands, with a team dedicated to helping you achieve your digital objectives and drive business growth.
